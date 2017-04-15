796ba96c5bd6241b2f6a47ac16ae2531ba96b1ac
lironglong <1029398168@qq.ocm>
2017-04-15 14:39:56 +0800

    增加了一个展示的textview。

---
+++ app/src/main/java/com/lironglong/administrator/testgitdemo/SecondAvtivity.java
@@ -0,0 +1,13 @@
+package com.lironglong.administrator.testgitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_avtivity);
+    }
+}
